c93bcf8b5e7bc394ca963903ea0f21c42805ff49
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-18 13:30:34 +0800

    Update `MainProgram` to include lesson 1 (Hello World + Draw A Shape), and a `Console.ReadLine()` command to only terminate a program after pressing `Enter`.

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -12,7 +12,17 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
+            /* Lesson 01 (Hello World + Draw A Shape) */
+            Console.WriteLine("* Lesson 01 (Hello World And Draw A Shape) *");
+            // (Hello World) Print line to console.
+            Console.WriteLine("Hello World!");
+            // (Draw A Shape) Lines printed starts from top to bottom.
+            Console.WriteLine("00100");
+            Console.WriteLine("01110");
+            Console.WriteLine("11111");
             
+            /* End Execution */
+            Console.ReadLine();  // Keep console up to see what is in the console before termination (useful for VS Code)
         }
     }
 }
