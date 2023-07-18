b6ac732aa9ee7c69ab7da6d5f58647bff6093924
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-18 15:49:19 +0800

    Update `MainProgram` to include lesson 3 (Data Types + Strings + Numbers), and comment out the last `Console.ReadLine()` call to end the console early.

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -50,8 +50,53 @@ namespace CSharpTutorial
             Console.WriteLine("He is currently " + characterAge + " years old.");
             Console.WriteLine("He currently glows a/an " + characterAura + " aura.");
             
+            Console.WriteLine();
+            
+            /* Lesson 03 (Data Types + Strings + Numbers) */
+            Console.WriteLine("* Lesson 03 (Data Types + Strings + Numbers) *");
+            // (Data Types) Store different types of data.
+            string phrase = "You either die a hero, or live long enough to see yourself become a villain!";  // String, multi-length text.
+            char grade = 'A';  // Character, length of 1 as opposed to multi-length with Strings.
+            int age = 21;  // Integer, no need quotation marks, including all types of numbers, can handle negative numbers.
+            decimal debt = -3000.0m;  // Decimal, most precise non-integer number.
+            float height = 1.7f;  // Float, most energy-saving non-integer number.
+            double gpa = 6.42;  // Double, most used non-integer number, between decimal and float.
+            bool hasPassed = true;  // Boolean, only two values for yes (true) or no (false).
+            Console.WriteLine("Passing status: " + hasPassed + ".");
+            // (Strings) Deal with strings.
+            Console.WriteLine("Hello John!");  // Standard.
+            Console.WriteLine("Line1\nLine2");  // End line.
+            Console.WriteLine("\"It\'s Time!\"");  // Special characters.
+            Console.WriteLine(phrase);  // Using variables.
+            Console.WriteLine("Listen here! " + phrase);  // Concatenation.
+            Console.WriteLine("Length of phrase: " + phrase.Length + ".");  // Length of String.
+            Console.WriteLine(phrase.ToUpper());  // Letter casing (another example being `.lower()`).
+            Console.WriteLine(phrase.ToLower().Contains("villain"));  // Check if a string contains a substring.
+            Console.WriteLine(phrase.ToUpper().Contains("villain"));
+            Console.WriteLine(phrase[0]);  // Print character of an index (index within brackets).
+            Console.WriteLine(phrase.IndexOf("villain"));  // Print position of string and character (return -1 if not found).
+            Console.WriteLine(phrase.IndexOf(grade));
+            Console.WriteLine(phrase.Substring(68));  // Print out a substring from a string from a starting point (include length if needed).
+            Console.WriteLine(phrase.Substring(68, 7));
+            // (Numbers) Deal with numbers.
+            Console.WriteLine(50);  // Standard.
+            Console.WriteLine(13 % 8);  // Math calculations (+, -, *, /, %, etc.).
+            Console.WriteLine(4 + 2 * 3);  // B.O.D.M.A.S (order of operation).
+            Console.WriteLine(age);  // Print variable value.
+            age++;  // Increment.
+            Console.WriteLine(age);
+            age--;  // Decrement.
+            Console.WriteLine(age);  // Print quotient from division (only return Integer).
+            Console.WriteLine(gpa / 2.0);  // Print value from true division (returns a non-Integer).
+            Console.WriteLine(Math.Abs(debt));  // Print absolute value.
+            Console.WriteLine(Math.Pow(2, 3));  // Print power.
+            Console.WriteLine(Math.Sqrt(25));  // Print square root.
+            Console.WriteLine(Math.Max(height, 1.6f));  // Print maximum or minimum of two numbers.
+            Console.WriteLine(Math.Min(height, 1.8f));
+            Console.WriteLine(Math.Round(4.5));  // Print nearest whole number.
+            
             /* End Execution */
-            Console.ReadLine();  // Keep console up to see what is in the console before termination (useful for VS Code)
+            // Console.ReadLine();  // Keep console up to see what is in the console before termination (useful for VS Code)
         }
     }
 }
