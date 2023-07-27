ed7affdb3d8db0b6b878d15fdce18e8c36a705ca
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-27 13:42:10 +0800

    Complete lesson 1 in `CPlusPlusTutorial.cpp`.

--- CPlusPlusTutorial/CPlusPlusTutorial.cpp
+++ CPlusPlusTutorial/CPlusPlusTutorial.cpp
@@ -1,6 +1,28 @@
+#include <iostream>
 
-int main(int argc, char* argv[])
+using namespace std;
+
+void lesson01()
 {
-    
+    /**
+     * Lesson 1: Hello World + Draw A Shape
+     */
+    cout << "Lesson 1: Hello World + Draw A Shape" << endl;
+    // (Hello World + Draw A Shape) Hello World.
+    cout << "Hello World!" << endl;
+    // (Hello World + Draw A Shape) Draw A Shape.
+    cout << "   /|" << endl;
+    cout << "  / |" << endl;
+    cout << " /  |" << endl;
+    cout << "/___|" << endl;
+}
+
+int main()
+{
+    /**
+     * All lessons.
+     */
+    lesson01();
+    cout << "" << endl;
     return 0;
 }
