bbd466c349d06f62a8da12f99106d6d6cfcf4c31
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-27 14:09:58 +0800

    Complete lesson 2 in `CPlusPlusTutorial.cpp`.

--- CPlusPlusTutorial/CPlusPlusTutorial.cpp
+++ CPlusPlusTutorial/CPlusPlusTutorial.cpp
@@ -17,12 +17,44 @@ void lesson01()
     cout << "/___|" << endl;
 }
 
+void lesson02()
+{
+    /**
+     * Lesson 2: Variables + Data Types
+     */
+    cout << "Lesson 2: Variables + Data Types" << endl;
+    // (Variables + Data Types) Variables - Manual editing.
+    cout << "My name is KIRYU, I am 55 years old." << endl;
+    cout << "I have a RED aura, and my name is renamed to JORYU." << endl;
+    // (Variables + Data Types) Variables - Create variables and put them in the story, which the values can be updated.
+    string character_name = "KIRYU";
+    int character_age;
+    character_age = 55;
+    cout << "My name is " << character_name << ". I am " << character_age << " years old." << endl;
+    string character_aura = "RED";
+    character_name = "JORYU";
+    cout << "I have a " << character_aura << " aura, and my name is renamed to " << character_name << "." << endl;
+    // (Variables + Data Types) Data Types - Create different types.
+    char grade = 'A';  // Character (Single-length text).
+    cout << grade << endl;
+    string word = "Santos";  // String (Multi-length text).
+    cout << word << endl;
+    int age = 21;  // Integer (Non-decimal number).
+    cout << age << endl;
+    float height = 1.71f;  // Float (More efficient decimal number).
+    cout << height << endl;
+    double gpa = 6.42;  // Double (More accurate decimal number).
+    cout << gpa << endl;
+    bool is_male = true;  // Boolean (true or false).
+    cout << is_male << endl;
+}
+
 int main()
 {
     /**
      * All lessons.
      */
     lesson01();
-    cout << "" << endl;
+    lesson02();
     return 0;
 }
